ca8e1a5695907469cbe4d167e1153c56a7885032
NazimS <nazim.shixkerimov@gmail.com>
2022-10-22 23:35:56 +0400

    button events fully finished

--- script.js
+++ script.js
@@ -1,13 +1,28 @@
 
-let btns = document.getElementsByClassName("btn");
+    // color button event
+    let btns = document.getElementsByClassName("btn");
 
-for (let i=0; i < btns.length; i++) {
-    btns[i].addEventListener("click", function() {
-        let buttonStyle = getComputedStyle(this);
-        let buttonBgColor = buttonStyle["backgroundColor"];
-        document.body.style.backgroundColor = buttonBgColor;
-    });
+    for (let i=0; i < btns.length; i++) {
+        btns[i].addEventListener("click", function() {
+        
+            let buttonStyle = getComputedStyle(this);
+            let buttonBgColor = buttonStyle["backgroundColor"];
+            document.body.style.backgroundColor = buttonBgColor;
+            document.getElementById("title").style.color = "white";
+            document.getElementById("clear").style.backgroundColor = "white";
+  
+        });
+    }
 
-}
+    //reset button event
+        const clear = document.getElementById("clear");
+        
+        clear.addEventListener("click", function onClick(event) {
+
+            const title = document.getElementById("title");
+            title.style.color = "black";
+
+        });
+
 
 

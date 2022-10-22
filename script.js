0d4cea62c353f8e3a63c79c01cd204bdcf9a19fe
NazimS <nazim.shixkerimov@gmail.com>
2022-10-22 19:15:07 +0400

    click event finished

--- script.js
+++ script.js
@@ -2,7 +2,10 @@
 let btns = document.getElementsByClassName("btn");
 
 for (let i=0; i < btns.length; i++) {
-    btns[i].addEventListener("click", function(){
-        let buttonstyle = getComputedStyle(this);
-    })
+    btns[i].addEventListener("click", function() {
+        let buttonStyle = getComputedStyle(this);
+        let buttonBgColor = buttonStyle["backgroundColor"];
+        document.body.style.backgroundColor = buttonBgColor;
+    });
+    
 }
